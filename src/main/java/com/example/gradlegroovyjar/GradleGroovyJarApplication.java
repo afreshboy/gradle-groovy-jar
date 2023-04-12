b4793e0a0d173270336b22f7571a5a64453f3e83
songziming.me <songziming.me@bytedance.com>
2023-04-12 17:18:09 +0800

    fix

---
+++ src/main/java/com/example/gradlegroovyjar/GradleGroovyJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlegroovyjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGroovyJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleGroovyJarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlegroovyjar/GradleGroovyJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlegroovyjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleGroovyJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
